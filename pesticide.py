db3664eb98d9f01f174ab9ff45d55250be45bd92
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-01-02 00:40:36 -0700

    Create pesticide.py

---
+++ pesticide.py
@@ -0,0 +1,16 @@
+# Pesticide use in California analyse
+# https://www.kaggle.com/usgs/pesticide-use
+
+
+import pandas as pd
+import numpy as np
+
+
+def analysis ():
+    print ("Pesticide use analysis")
+
+
+if __name__ == "__main__":
+    analysis ()
+
+
