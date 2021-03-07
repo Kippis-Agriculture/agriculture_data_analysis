651967802bf6cbc3e3e2b79875cc7ac0848a48d2
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-03-06 18:57:00 -0600

    Update pesticide.py

--- pesticide.py
+++ pesticide.py
@@ -4,9 +4,9 @@
 # This dataset includes annual county-level pesticide use estimates for 423 pesticides 
 # (active ingredients) applied to agricultural crops grown in the contiguous United States.
 
-import pandas as pd
+import matplot.lib as plt
 import numpy as np
-
+import pandas as pd
 
 df_pest_2014 = pd.read_csv("../data/2014.csv")
 df_pest_2015 = pd.read_csv("../data/2015.csv")
